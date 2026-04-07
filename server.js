2192e1b30c125ed7df9e747fcbc52ffa686eabe7
Priyanka <pri.ppk@gmail.com>
2026-04-07 11:51:05 -0700

    Fix port and Mongo env variable

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.log(err));
 
 // Start server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
